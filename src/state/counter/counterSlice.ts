bb3a8b9b8432cd1d4d1a59e36d59ea46f0f95da2
Roge <rogemateos@hotmail.com>
2024-03-23 00:35:06 +0000

    Building-actions-and-reducers
    Connect. counter component to reduce , to dispatch some actions , you have to use use selector  then. you get the state an you can select anything

--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -13,10 +13,20 @@ const initialState: CounterState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
 
 });
 
-
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;
 
